a4f8d656799e3b626add57c24de77ab000c61514
Rourke Bradley <rourkebradley@gmail.com>
2020-05-11 20:17:43 +0100

    Update

--- tests/ListingBasicTest.php
+++ tests/ListingBasicTest.php
@@ -134,6 +134,41 @@ class ListingBasicTest extends TestCase
         $this->assertEquals("basic",$listing->getStatus());
     }
 
+    public function testNoImage()
+    {
+        $data = [
+            'id' => 1,
+            'title' => 'Test Title',
+            'image' => '',
+        ];
+        $listing = new ListingBasic($data);
+        $this->assertFalse($listing->getImage());
+
+    }
+
+    public function testFullPathImage()
+    {
+        $data = [
+            'id' => 1,
+            'title' => 'Test Title',
+            'image' => 'https://www.cascadiaphp.com/images/logo.svg',
+        ];
+        $listing = new ListingBasic($data);
+        $this->assertEquals($data['image'], $listing->getImage());
+
+    }
+    public function testBuildPathImage()
+    {
+        define('BASE_URL', '/');
+        $data = [
+            'id' => 1,
+            'title' => 'Test Title',
+            'image' => 'images/listings/1.png',
+        ];
+        $listing = new ListingBasic($data);
+        $this->assertEquals(BASE_URL.'/'.$data['image'], $listing->getImage());
+    }
+
 }
 
 
